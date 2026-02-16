c126c47ff04c5af4617d9e1c987302073c74218f
iKittyCode <gator.syon@gmail.com>
2026-02-15 22:20:50 -0500

    api_key env

--- findimages.py
+++ findimages.py
@@ -1,6 +1,9 @@
-import wikipedia
+import requests
 import json
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
+secret_key = os.getenv("SECRET_KEY")
 content = None
 newround1content = []
 try: 
@@ -12,10 +15,11 @@ try:
         resultlist = j["description"].split("(")
         j["name"] = resultlist[0]
         newround1content.append(j)
-        
+
 except Exception as e:
     print(f"An error occurred: {e}")
-
+for lab in newround1content: 
+    lab["name"]
 
 
 
