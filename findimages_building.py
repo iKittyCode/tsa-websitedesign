e2ab0595382696b214e842b38e00c3fdf8916052
iKittyCode <gator.syon@gmail.com>
2026-03-07 18:31:54 -0500

    added building scraper

---
+++ findimages_building.py
@@ -0,0 +1,45 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+from serpapi.google_search import GoogleSearch
+load_dotenv()
+secret_key = os.getenv("SECRET_KEY")
+content = None
+newround1content = []
+newround2content = []
+
+
+try: 
+    with open ("public/accurate_buildings.json", "r", encoding='utf-8') as f:
+        content = json.load(f)
+    
+    for j in content: 
+        
+        resultlist = j["description"].split(" ")
+        j["name"] = resultlist[0] +  " " + resultlist[1]+  " " + resultlist[2]+  " " + resultlist[3] + " Basking Ridge, NJ"
+        newround1content.append(j)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+for lab in newround1content: 
+    params = {
+    "engine": "google_images",
+    "q": lab["name"],
+    "location": "Basking Ridge, New Jersey, United States",
+    "google_domain": "google.com",
+    "hl": "en",
+    "gl": "us",
+    "api_key": secret_key
+    }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    images_results = results["images_results"]
+    lab["image"] = images_results[0]["original"]
+    newround2content.append(lab)
+with open ("public/accurate_buildings.json", "w" ) as f: 
+    json.dump(newround2content,f , indent=4)
+
+
+    
+
